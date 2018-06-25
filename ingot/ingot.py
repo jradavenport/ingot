20ad4de3ea253b95896299871350776457eaa926
James Davenport <jradavenport@gmail.com>
2018-06-25 14:59:31 -0700

    update docstring and relative location of needed file

--- ingot/ingot.py
+++ ingot/ingot.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsRegressor
 from scipy.stats import binned_statistic
+from os.path import dirname,realpath
 
 
 def FeH(w1w2, GJ, MG, use_MG = False):
@@ -15,9 +16,33 @@ def FeH(w1w2, GJ, MG, use_MG = False):
     original training data.
 
     Optionally (recommended!): Include the Gaia Absolute G mag in the KNN model
+
+    Parameters
+    ----------
+    w1w2: float or numpy array
+        the (W1-W2) WISE color of stars to estimate [Fe/H] for
+    GJ: float or numpy array
+        the (G-J) Gaia-2MASS color of stars to estimate [Fe/H] for
+    MG: float or numpy array, optional
+        the absolute G-band Gaia magnitude of stars to estimate [Fe/H] for.
+        Not used by default.
+    use_MG: bool, optional
+        Should we use `MG` in computing the [Fe/H]? The results are slightly
+        better if True, but not strictly needed. Default is False
+
+    Returns
+    -------
+    FeH_preict: float or numpy array
+        the predicted [Fe/H] value for these stars based on the KNN regessor
+    FeH_predict_err: float or numpy array
+        the estimated [Fe/H] errors for these stars based on the scatter in the
+        KNN prediction of the original training data (Warning: not robust!)
     '''
 
-    df = pd.read_csv('ingot_data.csv.gz')
+    # sniff out where this code is, so can point to relative location of the required file
+    dir = dirname(realpath(__file__))
+    # read in the required training data that comes shipped w/ ingot
+    df = pd.read_csv(dir + '/ingot_data.csv.gz')
 
     if use_MG:
         Xdata = np.array([df['w1'].values - df[u'w2'].values,
